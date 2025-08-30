e5df8362b4f6629205e3e5da13655210ad289db0
장현우 <zzanghyunmoo@gmail.com>
2025-08-30 22:51:32 +0900

    Quartz sync: Aug 30, 2025, 10:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "zzanghyunmoo.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", "docs", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
